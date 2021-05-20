b302aecd2a834f261217bb96bff0af48a09554e7
R11Alpha <canchemario11@gmail.com>
2021-05-20 13:46:45 -0500

    Archivos

---
+++ unit1/a1.py
@@ -0,0 +1,10 @@
+print("This is a great program xD\n")
+
+x1 = int(input("Introduce the first number\n"))
+x2 = int(input("Introduce the second number\n"))
+
+sum = x1 + x2
+mul = x1 * x2
+
+print("The sum is: {}".format(sum))
+print("The multiply is: {}".format(mul))
