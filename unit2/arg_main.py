c287fa861f7a5354bd87c85979a43bbed3ecd7da
R11Alpha <canchemario11@gmail.com>
2021-05-27 18:00:16 -0500

    Quiz

---
+++ unit2/arg_main.py
@@ -0,0 +1,19 @@
+import sys
+
+##Variables Globales##
+
+#my_int = 0
+#my_str = ""
+#my_float = 0.0
+#my_bool = True ##False
+#my_list = [] ##list
+
+if __name__ == "__main__":
+
+    my_int = 12
+    my_str = "Hola mundo"
+    my_float = 3.14
+    my_bool = False ##False
+    my_list = [1,2,3,4,5] ##list
+
+    print(my_int, my_str, my_float, my_bool, my_list)
